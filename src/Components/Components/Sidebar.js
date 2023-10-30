2fdb913e50bc1543592365024eb4478bca5b1eab
askari-fahlyani <askari.fahlyani@gmail.com>
2023-10-30 18:49:29 +0100

    icons added to project

--- src/Components/Components/Sidebar.js
+++ src/Components/Components/Sidebar.js
@@ -1,24 +1,35 @@
 import React from "react";
 import "./Sidebar.css";
+import { AiOutlineHome } from "react-icons/ai";
+import {MdProductionQuantityLimits} from 'react-icons/md'
+import {LiaComments} from 'react-icons/lia'
+import {FiUsers} from 'react-icons/fi'
+import {BsBagCheck} from 'react-icons/bs'
 
 function Sidebar() {
+    const iconSize = '24px'
   return (
     <div className="sidebar">
       <h1 className="sidebar-title">welcome to your dashboard</h1>
       <ul className="sidebar-links">
         <li>
+          <AiOutlineHome size={iconSize} />
           <a href="#">HOME</a>
         </li>
         <li className="active">
+            <MdProductionQuantityLimits size={iconSize}/>
           <a href="#">PRODUCTS</a>
         </li>
         <li>
+            <LiaComments size={iconSize}/>
           <a href="#">COMMENTS</a>
         </li>
         <li>
+            <FiUsers size={iconSize}/>
           <a href="#">USERS</a>
         </li>
         <li>
+            <BsBagCheck size={iconSize}/>
           <a href="#">ORDERS</a>
         </li>
       </ul>
